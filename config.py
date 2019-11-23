5f34114ddd855e0dce136857bebcd1f2a3b7105f
Gichimu <gichimueric12@gmail.com>
2019-11-23 13:48:59 +0300

    initial commit

---
+++ config.py
@@ -0,0 +1,21 @@
+class Config:
+
+
+
+    pass
+
+class ProdConfig:
+
+    pass
+
+
+
+class DevConfig:
+
+    DEBUG = True
+
+
+config_options{
+    'production':ProdConfig,
+    'development':DevConfig
+}
